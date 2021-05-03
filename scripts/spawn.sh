dc598bd6624b1cac381149901cd6b8c52eb80d61
Ali Asgari <altostratous@gmail.com>
2021-05-03 08:15:56 -0700

    Remove randomness

--- scripts/spawn.sh
+++ scripts/spawn.sh
@@ -4,7 +4,7 @@ mkdir -p generated
 for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; do
     if [[ $b != "gap" ]]; then continue; fi
     TRACE_PATH=ML-DPC/ChampSimTraces/$b/$t;
-    if [[ `python -c "from random import random; print(random() < 0.1)"` = "True" ]]; then
+    # if [[ `python -c "from random import random; print(random() < 0.1)"` = "True" ]]; then
         for bucket in page ip; do
             for lookahead in 0 4 8; do
                 export BUCKET=$bucket
@@ -12,5 +12,5 @@ for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; 
                 echo sbatch --export=ALL --mem 1G --time 3:00:00 scripts/single.sh ./ml_prefetch_sim.py run $TRACE_PATH --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-$bucket-$lookahead" --no-base --prefetch "generated/$bucket-$lookahead.txt"
             done
         done
-    fi
+    # fi
 done; done
