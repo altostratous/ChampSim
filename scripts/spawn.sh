bbde025d7fa69447843ac8e5f72c983f33aa66b3
Ali Asgari <altostratous@gmail.com>
2021-05-03 08:21:33 -0700

    Make spawn.sh multi purpose

--- scripts/spawn.sh
+++ scripts/spawn.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+$TRACE_TYPE='ChampSimTraces'
+$ML_COMMAND=train
+
 mkdir -p generated
-for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; do
+for b in `ls ML-DPC/$TRACE_TYPE`; do for t in `ls ML-DPC/$TRACE_TYPE/$b`; do
     if [[ $b != "gap" ]]; then continue; fi
-    TRACE_PATH=ML-DPC/ChampSimTraces/$b/$t;
+    TRACE_PATH=ML-DPC/$TRACE_TYPE/$b/$t;
     # if [[ `python -c "from random import random; print(random() < 0.1)"` = "True" ]]; then
         for bucket in page ip; do
             for lookahead in 0 4 8; do
                 export BUCKET=$bucket
                 export LOOKAHEAD=$lookahead
-                echo sbatch --export=ALL --mem 1G --time 3:00:00 scripts/single.sh ./ml_prefetch_sim.py run $TRACE_PATH --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-$bucket-$lookahead" --no-base --prefetch "generated/$bucket-$lookahead.txt"
+                PREFETCH_FILE="generated/$bucket-$lookahead.txt"
+                if [[ $ML_COMMAND = "run" ]]; then
+                    echo sbatch --export=ALL --mem 1G --time 3:00:00 scripts/single.sh ./ml_prefetch_sim.py run $TRACE_PATH --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-$bucket-$lookahead" --no-base --prefetch $PREFETCH_FILE
+                else
+                    echo sbatch --export=ALL --mem 1G --time 3:00:00 scripts/single.sh ./ml_prefetch_sim.py train $TRACE_PATH --num-prefetch-warmup-instructions 100 --num-instructions 100 --generate $PREFETCH_FILE
             done
         done
     # fi
