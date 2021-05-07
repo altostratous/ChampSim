6eb25f3ad2b158740c960068edd31a3e3d1c6023
Ali Asgari <altostratous@gmail.com>
2021-05-07 14:03:58 -0700

    Fix syntax

--- scripts/bo_spawn.sh
+++ scripts/bo_spawn.sh
@@ -12,10 +12,10 @@ for b in `ls ML-DPC/$TRACE_TYPE`; do for t in `ls ML-DPC/$TRACE_TYPE/$b`; do
     export ML_MODEL_NAME=BestOffset
     export FUZZY_BO=True
     # --gres=gpu:p100:1
-    JOB_ID=`sbatch --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py train $TRACE_PATH --num-prefetch-warmup-instructions 100 --generate $PREFETCH_FILE
+    JOB_ID=`sbatch --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py train $TRACE_PATH --num-prefetch-warmup-instructions 100 --generate $PREFETCH_FILE`
     SUCCEEDED=$?
     if [[ $SUCCEEDED = 0 ]]; then
       JOB_ID=`echo $JOB_ID | awk '{print $4}'`
-      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-bo" --prefetch $PREFETCH_FILE --no-base
+      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-bo" --prefetch $PREFETCH_FILE  --no-base
     fi
 done; done
