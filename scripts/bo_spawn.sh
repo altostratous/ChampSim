7320b3762e9660288a477dce168089ea751cc1fc
Ali Asgari <altostratous@gmail.com>
2021-05-07 13:35:39 -0700

    Make bo_evaluation pipeline

--- scripts/bo_spawn.sh
+++ scripts/bo_spawn.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-TRACE_TYPE='ChampSimTraces'
-#TRACE_TYPE='LoadTraces'
-ML_COMMAND=run
-#ML_COMMAND=train
+TRACE_TYPE='LoadTraces'
 
 
 mkdir -p generated
@@ -11,14 +8,14 @@ for b in `ls ML-DPC/$TRACE_TYPE`; do for t in `ls ML-DPC/$TRACE_TYPE/$b`; do
     # if [[ $b != "spec06" ]]; then continue; fi
     TRACE_PATH=ML-DPC/$TRACE_TYPE/$b/$t;
     PREFETCH_FILE="generated/$b-$t-bo.txt"
+    PREFETCH_FILE=`echo $PREFETCH_FILE | sed 's/trace.gz/txt.xz/g'`
     export ML_MODEL_NAME=BestOffset
-    if [[ $ML_COMMAND = "run" ]]; then
-        PREFETCH_FILE=`echo $PREFETCH_FILE | sed 's/trace.gz/txt.xz/g'`
-        if [[ -f $PREFETCH_FILE ]]; then
-            sbatch --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run $TRACE_PATH --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-bo" --prefetch $PREFETCH_FILE
-        fi
-    else
-        # --gres=gpu:p100:1
-        sbatch --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py train $TRACE_PATH --num-prefetch-warmup-instructions 100 --generate $PREFETCH_FILE --no-base
+    export FUZZY_BO=True
+    # --gres=gpu:p100:1
+    JOB_ID=`sbatch --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py train $TRACE_PATH --num-prefetch-warmup-instructions 100 --generate $PREFETCH_FILE --no-base`
+    SUCCEEDED=$?
+    if [[ $SUCCEEDED = 0 ]]; then
+      JOB_ID=`echo $JOB_ID | awk '{print $4}'`
+      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-bo" --prefetch $PREFETCH_FILE
     fi
 done; done
