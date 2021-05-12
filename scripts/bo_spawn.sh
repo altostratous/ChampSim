14266d70f0cb468fe6783a197a7585f90478fc67
Ali Asgari <altostratous@gmail.com>
2021-05-12 14:04:58 -0700

    Let no --no-base

--- scripts/bo_spawn.sh
+++ scripts/bo_spawn.sh
@@ -4,7 +4,7 @@
 for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; do
     TRACE_PATH=ML-DPC/ChampSimTraces/$b/$t;
     LOAD_TRACE_PATH=ML-DPC/LoadTraces/$b/`echo $t | sed 's/trace.xz/txt.xz/g' | sed 's/trace.gz/txt.xz/g'`;
-    export ML_MODEL_NAME=Memento
+    export ML_MODEL_NAME=MementoModel
     export FUZZY_BO=True
     export BO_SCORE_SCALE=$scale
     export MEMENTO_DELAY=0
@@ -20,7 +20,7 @@ for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; 
     SUCCEEDED=$?
     if [[ $SUCCEEDED = 0 ]]; then
       JOB_ID=`echo $JOB_ID | awk '{print $4}'`
-      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-memento$MEMENTO_DELAY" --prefetch $PREFETCH_FILE --no-base
+      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-memento$MEMENTO_DELAY" --prefetch $PREFETCH_FILE
     fi
     if [[ -f $TRACE_PATH ]] && [[ -f $LOAD_TRACE_PATH ]]; then echo OK; fi
     exit
