0d6468c9639fc3dc516626d25ce5ffe6d876bbcc
Ali Asgari <altostratous@gmail.com>
2021-05-14 10:55:44 -0700

    Fix bo_spawn to test debug cnn

--- scripts/bo_spawn.sh
+++ scripts/bo_spawn.sh
@@ -8,7 +8,11 @@ for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; 
     export FUZZY_BO=True
     export BO_SCORE_SCALE=1
     export MEMENTO_DELAY=1
-    GENERATED_DIR="generated-memento-$MEMENTO_DELAY"
+    export ML_MODEL_NAME=TerribleMLModel
+    export LOOKAHEAD=5
+    export BUCKET=ip
+    export EPOCHS=20
+    GENERATED_DIR="generated-debugcnn-$LOOKAHEAD"
     mkdir -p $GENERATED_DIR
     PREFETCH_FILE="$GENERATED_DIR/$b-$t-memento.txt"
     PREFETCH_FILE=`echo $PREFETCH_FILE | sed 's/trace.gz/txt.xz/g'`
@@ -20,7 +24,7 @@ for b in `ls ML-DPC/ChampSimTraces`; do for t in `ls ML-DPC/ChampSimTraces/$b`; 
     SUCCEEDED=$?
     if [[ $SUCCEEDED = 0 ]]; then
       JOB_ID=`echo $JOB_ID | awk '{print $4}'`
-      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-memento$MEMENTO_DELAY" --prefetch $PREFETCH_FILE
+      sbatch --dependency=afterok:$JOB_ID --export=ALL --mem 10G --time 6:00:00 scripts/single.sh ./ml_prefetch_sim.py run ML-DPC/ChampSimTraces/$b/$t --num-prefetch-warmup-instructions 100 --num-instructions 100 --results-dir="results-debugcnn-$LOOKAHEAD" --prefetch $PREFETCH_FILE
     fi
     if [[ -f $TRACE_PATH ]] && [[ -f $LOAD_TRACE_PATH ]]; then echo OK; fi
     exit
